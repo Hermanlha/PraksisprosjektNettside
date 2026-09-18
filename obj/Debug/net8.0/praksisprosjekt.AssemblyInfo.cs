574f1bd9d76b0b1ef53831960ccf759162a56306
hldkjft <hermanlha@uia.no>
2026-09-18 21:59:34 +0200

    Navnretting

--- obj/Debug/net8.0/praksisprosjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/praksisprosjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("praksisprosjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb713639069597b96cdcb3763e8e8e66b8f0e84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39576d237c2d51c9dc5b900dcec59f5d636ae531")]
 [assembly: System.Reflection.AssemblyProductAttribute("praksisprosjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("praksisprosjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
